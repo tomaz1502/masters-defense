5c251e395a93d3c90dbfe65a7127f3662e063ab9
tomaz1502 <tomgm1502@gmail.com>
2023-09-12 11:18:28 -0300

    more update

--- .auctex-auto/talk.el
+++ .auctex-auto/talk.el
@@ -5,14 +5,14 @@
                      '(("beamer" "usepdftitle=false" "aspectratio=169" "usenames" "dvipsnames")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("datetime" "yyyymmdd") ("biblatex" "backend=bibtex" "doi=false" "isbn=false" "url=false" "style=alphabetic" "maxalphanames=4" "maxcitenames=3" "minalphanames=3" "minnames=3")))
-   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
